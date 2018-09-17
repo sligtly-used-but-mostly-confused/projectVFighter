dc9ae64acec60837778558a1e4326d181c403d20
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-09-17 08:57:50 -0500

    asd

--- VFighter/Assets/Scripts/GamepadPlayerController.cs
+++ VFighter/Assets/Scripts/GamepadPlayerController.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyboardPlayerController : PlayerController
-{
+public class GamepadPlayerController : PlayerController {
     [SerializeField]
     private int _inputDevice = 2;
 
     void Update()
-    { 
+    {
         var inputDevice = MappedInput.InputDevices[_inputDevice];
 
         float mouseX = inputDevice.GetAxisRaw(MappedAxis.AimX);
@@ -18,6 +17,7 @@ public class KeyboardPlayerController : PlayerController
         float ChangeGravY = inputDevice.GetAxis(MappedAxis.Vertical);
 
         Vector2 changeGravDir = new Vector2(ChangeGravX, ChangeGravY);
+        Debug.Log(changeGravDir);
         //Debug.Log(changeGravDir);
         var mousePos = Camera.main.ScreenToWorldPoint(new Vector2(mouseX, mouseY));
         var deltaFromPlayer = mousePos - transform.position;
@@ -25,7 +25,7 @@ public class KeyboardPlayerController : PlayerController
         AimReticle(deltaFromPlayer);
 
         //if(inputDevice.GetButtonDown(MappedButton.ChangeGrav))
-        if(changeGravDir != Vector2.zero)
+        if (changeGravDir != Vector2.zero)
         {
             var closestDir = ClosestDirection(changeGravDir.normalized);
             ChangeGravity(closestDir);
