045560192776c037326f78c2d2d2e3ff61a2939e
Austin Miller <Austin@main-10-230-166-171.tamulink.tamu.edu>
2018-09-18 09:45:49 -0500

    Added ForceZone Prefab

--- VFighter/Assets/ForceZoneController.cs
+++ VFighter/Assets/ForceZoneController.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForceZoneController : MonoBehaviour {
+public class ForceZoneController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector2 gravityForce = new Vector2(80f, -60f);
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        collision.attachedRigidbody.AddForce(transform.TransformDirection(gravityForce), ForceMode2D.Force);
+        Debug.Log("adding force");
+    }
 }
