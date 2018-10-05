82e70743d095ae4f3152ecb7343aa7c8c6a0dbf5
Juan Carlos <sandoval1302@tamu.edu>
2018-10-05 15:33:37 -0500

    added PixelScene

---
+++ VFighter/Assets/Scripts/PixelateImageEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelateImageEffect : MonoBehaviour {
+    public Material effectMaterial;
+    int number = 5;
+	// Use this for initialization
+    [ExecuteInEditMode]
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture  dst)
+    {
+        Graphics.Blit(src, dst, effectMaterial);
+    }
+}
