b92177d454dc1e0360d9d06adc45e2244702c712
Austin Miller <Austin@main-10-231-212-183.tamulink.tamu.edu>
2018-10-08 08:43:18 -0500

    Added programatic platform building

--- VFighter/Assets/Scripts/LevelSelectManager.cs
+++ VFighter/Assets/Scripts/LevelSelectManager.cs
@@ -4,77 +4,70 @@ using UnityEngine;
 
 
 
-public class LevelSelectManager : MonoBehaviour {
+public class LevelSelectManager : MonoBehaviour
+{
 
     public List<string> levels;
     public GameObject platform;
     public GameObject levelZone;
+    public float areaWidth;
 
     private List<LevelZoneController> zones = new List<LevelZoneController>();
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         SpawnLevelPlatforms();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-    void SpawnLevelPlatforms(){
+    }
+
+    void SpawnLevelPlatforms()
+    {
 
         int count = levels.Count;
         int levelsRemaining = count;
         int levelIndex = 0;
+        int platforms = (int)Mathf.Floor(((float)count) / 2);
+        float gap = areaWidth / (platforms + 1);
+        float xValue = transform.position.x - areaWidth / 2;
 
-        if (count > 4)
-        { // 6 and 5
-            for (int x = -4; x <= 4; x += 4)
+        while (levelsRemaining > 0)
+        {
+            xValue += gap;
+            Vector3 pos = new Vector3(xValue, 0, 0);
+            Instantiate(platform, pos, Quaternion.identity);
+
+            if (levelsRemaining > 1)
             {
-                Vector3 pos = new Vector3(x, 0, 0);
-                Instantiate(platform, pos, Quaternion.identity);
+                //instantiate the levels
 
-                if (levelsRemaining > 1)
+
+                for (int y = -1; y < 2; y += 2)
                 {
-                    //instantiate the levels
-
-
-                    for (int y = -1; y < 2; y += 2){
-                        Vector3 zonePos = pos;
-                        zonePos += new Vector3(0, y * 1.25f, 0);
-                        LevelZoneController zone = Instantiate(levelZone, zonePos, Quaternion.identity).GetComponent<LevelZoneController>();
-                        zones.Add(zone);
-                        zone.levelName = levels[levelIndex];
-                        ++levelIndex;
-                        --levelsRemaining;
-                    }
-
-                    //set the level names
-                    //put them in zones list
-                    //update variables
-                }
-                else if (levelsRemaining == 1){
                     Vector3 zonePos = pos;
-                    zonePos += new Vector3(0, 1.25f, 0);
+                    zonePos += new Vector3(0, y * 1.25f, 0);
                     LevelZoneController zone = Instantiate(levelZone, zonePos, Quaternion.identity).GetComponent<LevelZoneController>();
                     zones.Add(zone);
                     zone.levelName = levels[levelIndex];
                     ++levelIndex;
                     --levelsRemaining;
                 }
-
             }
-        }
-        else
-        { //1-4
-            for (int x = -4; x <= 4; x += 8)
+            else if (levelsRemaining == 1)
             {
-                Vector3 pos = new Vector3(x, 0, 0);
-                Instantiate(platform, pos, Quaternion.identity);
+                Vector3 zonePos = pos;
+                zonePos += new Vector3(0, 1.25f, 0);
+                LevelZoneController zone = Instantiate(levelZone, zonePos, Quaternion.identity).GetComponent<LevelZoneController>();
+                zones.Add(zone);
+                zone.levelName = levels[levelIndex];
+                ++levelIndex;
+                --levelsRemaining;
             }
         }
     }
-
 }
