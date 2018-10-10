f9b23bb8363a3a5ecf6c0731f7f604b21280ef10
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-10-10 09:40:37 -0500

    setup

--- VFighter/Assets/Scripts/EffectObjects/ForceZoneController.cs
+++ VFighter/Assets/Scripts/EffectObjects/ForceZoneController.cs
@@ -5,22 +5,24 @@ using System.Linq;
 
 public class ForceZoneController : MonoBehaviour
 {
-    public Vector2 gravityForce = new Vector2(80f, -60f);
+    public Vector2 gravityDirection = new Vector2(80f, -60f);
+    public float Force = 10;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.GetComponent<GravityObjectRigidBody>())
         {
-            collision.GetComponent<GravityObjectRigidBody>().AddVelocity(VelocityType.OtherPhysics, gravityForce);
+            collision.GetComponent<GravityObjectRigidBody>().AddVelocity(VelocityType.OtherPhysics, gravityDirection.normalized * Force);
 
             var players = FindObjectsOfType<PlayerController>().Where(x => x.isLocalPlayer);
             if(players.Count() > 0)
             {
-                players.First().ChangeGORBGravityDirection(collision.GetComponent<GravityObjectRigidBody>(), gravityForce.normalized);
+                players.First().ChangeGORBGravityDirection(collision.GetComponent<GravityObjectRigidBody>(), gravityDirection.normalized);
             }
         }
     }
 
     public Vector2 GetgravityForce(){
-        return gravityForce;
+        return gravityDirection;
     }
 }
