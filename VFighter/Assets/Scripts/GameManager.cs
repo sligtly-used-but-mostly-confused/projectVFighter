26a08b680820517226dab776d6efad54a86665d0
Juan Carlos <sandoval1302@tamu.edu>
2018-10-10 08:52:37 -0500

    added

--- VFighter/Assets/Scripts/GameManager.cs
+++ VFighter/Assets/Scripts/GameManager.cs
@@ -13,7 +13,8 @@ public class GameManager : NetworkBehaviour {
 
     [SerializeField]
     private string _levelName;
-    
+    public bool DebugScene = false;
+
     public bool CurrentlyChangingScenes = false;
     public float ProgressionThroughGame = 1;
 
@@ -41,16 +42,16 @@ public class GameManager : NetworkBehaviour {
     {
         var players = FindObjectsOfType<PlayerController>().ToList();
         var alive = players.Where(x => { return (x.ControlledPlayer.NumLives - x.ControlledPlayer.NumDeaths) > 0; }).ToList();
-        
-        if(alive.Count() <= 1)
+
+        if (alive.Count() <= 1 && !DebugScene)
         {
             CheckHeartBeatThenCallback(() =>
             {
-                //ControllerSelectManager.Instance.Init();
                 players.ForEach(x => x.ControlledPlayer.Reset());
                 CurrentlyChangingScenes = true;
                 NetworkManager.singleton.ServerChangeScene(LevelSelect);
             });
+
         }
         else
         {
