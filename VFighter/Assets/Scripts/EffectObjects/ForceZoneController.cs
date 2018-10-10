893792553657d1e5dc51bc36f87f4df51bffa960
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-10-10 11:47:33 -0500

    fixed force zone

--- VFighter/Assets/Scripts/EffectObjects/ForceZoneController.cs
+++ VFighter/Assets/Scripts/EffectObjects/ForceZoneController.cs
@@ -5,20 +5,30 @@ using System.Linq;
 
 public class ForceZoneController : MonoBehaviour
 {
-    public Vector2 gravityDirection = new Vector2(80f, -60f);
-    public float Force = 10;
+    public Vector2 gravityDirection = new Vector2(1, 1);
+    public float Force = 4f;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+
         if (collision.GetComponent<GravityObjectRigidBody>())
         {
-            collision.GetComponent<GravityObjectRigidBody>().AddVelocity(VelocityType.OtherPhysics, gravityDirection.normalized * Force);
-
+            var changeInGravDirection = gravityDirection.normalized - collision.GetComponent<GravityObjectRigidBody>().GravityDirection;
+            var newGravDirection = collision.GetComponent<GravityObjectRigidBody>().GravityDirection + changeInGravDirection * Force * Time.deltaTime;
             var players = FindObjectsOfType<PlayerController>().Where(x => x.isLocalPlayer);
-            if(players.Count() > 0)
+            if (players.Count() > 0)
             {
-                players.First().ChangeGORBGravityDirection(collision.GetComponent<GravityObjectRigidBody>(), gravityDirection.normalized);
+                players.First().ChangeGORBGravityDirection(collision.GetComponent<GravityObjectRigidBody>(), newGravDirection);
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var players = FindObjectsOfType<PlayerController>().Where(x => x.isLocalPlayer);
+        if (players.Count() > 0)
+        {
+            players.First().ChangeGORBGravityDirection(collision.GetComponent<GravityObjectRigidBody>(), gravityDirection.normalized);
         }
     }
 
