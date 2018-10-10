202388f243483c0891223a02e65a4bbb7bdb5838
Juan Carlos <sandoval1302@tamu.edu>
2018-10-10 10:38:50 -0500

    Add Sprite Gun Pulse

--- VFighter/Assets/Scripts/PlayerControllers/PlayerController.cs
+++ VFighter/Assets/Scripts/PlayerControllers/PlayerController.cs
@@ -319,11 +319,15 @@ public abstract class PlayerController : NetworkBehaviour {
     [Command]
     public void CmdSpawnProjectile(Vector2 dir, float secondsUntilDestroy, bool isFromShotgun)
     {
+        float xValue = dir.x;
+        float yVlaue = dir.y;
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x);
         GameObject projectileClone = Instantiate(ProjectilePrefab, Reticle.transform.position, Reticle.transform.rotation);
         projectileClone.GetComponent<GravityGunProjectileController>().Owner = this;
         projectileClone.GetComponent<GravityGunProjectileController>().SecondsUntilDestroy = secondsUntilDestroy;
         projectileClone.GetComponent<GravityGunProjectileController>().IsShotgunProjectile = isFromShotgun;
         projectileClone.GetComponent<GravityObjectRigidBody>().UpdateVelocity(VelocityType.OtherPhysics, dir * ShootSpeed);
+        projectileClone.transform.Rotate(0, 0, angle);
         NetworkServer.Spawn(projectileClone);
     }
 
