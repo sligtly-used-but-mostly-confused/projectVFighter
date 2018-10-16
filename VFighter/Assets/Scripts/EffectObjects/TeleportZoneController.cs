2a0fd291f77f9250f33603a9f8e84c916d082072
Ryan Chomistek <ryan.chomistek@tamu.edu>
2018-10-15 21:53:01 -0500

    made teleporter for stable

--- VFighter/Assets/Scripts/EffectObjects/TeleportZoneController.cs
+++ VFighter/Assets/Scripts/EffectObjects/TeleportZoneController.cs
@@ -5,32 +5,48 @@ using UnityEngine;
 public class TeleportZoneController : MonoBehaviour {
 
     public TeleportZoneController TeleportTo;
+    public List<GravityObjectRigidBody> ObjectsWaitingToExitTeleporter = new List<GravityObjectRigidBody>();
 
     [SerializeField]
-    private bool _waitingForCooldown;
+    public Vector2 ForceKickDirection = Vector2.zero;
+    [SerializeField]
+    public float ForceKickScale = 100;
 
-    public List<GravityObjectRigidBody> ObjectsWaitingToExitTeleporter = new List<GravityObjectRigidBody>();
+    [SerializeField]
+    private float _objectExitFromTeleporterTimeout = .1f;
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (!_waitingForCooldown)
+        Rigidbody2D rb = collision.attachedRigidbody;
+        var gorb = collision.GetComponent<GravityObjectRigidBody>();
+        if (gorb && !ObjectsWaitingToExitTeleporter.Contains(gorb))
         {
-            Rigidbody2D rb = collision.attachedRigidbody;
-            var gorb = collision.GetComponent<GravityObjectRigidBody>();
-            if (gorb && !ObjectsWaitingToExitTeleporter.Contains(gorb))
-            {
-                TeleportTo.ObjectsWaitingToExitTeleporter.Add(gorb);
+            Debug.Log("teleporting");
+            TeleportTo.ObjectsWaitingToExitTeleporter.Add(gorb);
 
-                //get the players offset from the teleporter so we can match it on the other side
-                //also do a projection with the scale of the teleporter so that the player is garunteed to spawn 
-                //within the teleporter when they come out
-                var deltaPosition = collision.transform.position - transform.position;
-                var teleporterScale = TeleportTo.transform.rotation * TeleportTo.transform.lossyScale;
-                teleporterScale = new Vector3(Mathf.Abs(teleporterScale.x), Mathf.Abs(teleporterScale.y), Mathf.Abs(teleporterScale.z));
-                var playerOffsetFromTeleporter = Vector3.Project(deltaPosition, teleporterScale);
+            //get the players offset from the teleporter so we can match it on the other side
+            //also do a projection with the scale of the teleporter so that the player is garunteed to spawn 
+            //within the teleporter when they come out
+            var deltaPosition = collision.transform.position - transform.position;
+            var teleporterScale = TeleportTo.transform.rotation * TeleportTo.transform.lossyScale;
+            teleporterScale = new Vector3(Mathf.Abs(teleporterScale.x), Mathf.Abs(teleporterScale.y), Mathf.Abs(teleporterScale.z));
+            var playerOffsetFromTeleporter = Vector3.Project(deltaPosition, teleporterScale);
 
-                collision.transform.position = TeleportTo.transform.position + playerOffsetFromTeleporter;
-            }
+            gorb.AddVelocity(VelocityType.OtherPhysics, TeleportTo.ForceKickDirection * TeleportTo.ForceKickScale);
+            collision.transform.position = TeleportTo.transform.position + playerOffsetFromTeleporter;
+
+            TeleportTo.StartCoroutine(TeleportTo.ObjectExitTeleporterTimeout(gorb));
+        }
+    }
+
+    public IEnumerator ObjectExitTeleporterTimeout(GravityObjectRigidBody gorb)
+    {
+        yield return new WaitForSeconds(_objectExitFromTeleporterTimeout);
+
+        //unity derped and didnt trigger the on trigger exit
+        if (gorb && gorb.isActiveAndEnabled && ObjectsWaitingToExitTeleporter.Contains(gorb))
+        {
+            ObjectsWaitingToExitTeleporter.Remove(gorb);
         }
     }
 
