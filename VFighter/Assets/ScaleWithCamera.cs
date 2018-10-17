37e81bc2606a9dbe569c7fff62bee9a2be0102dc
PM-Vielma <pablom.vielma1130@gmail.com>
2018-10-17 09:01:06 -0500

    cam test

---
+++ VFighter/Assets/ScaleWithCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleWithCamera : MonoBehaviour {
+
+    public Transform tran;
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //tran.localScale.x = 18 * (double)cam.orthographicSize;
+        //tran.localScale.y = 10.125 * (double)cam.orthographicSize;
+	}
+}
