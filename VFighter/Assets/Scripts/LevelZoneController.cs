d6935c8f7e1f962c2ce3c885cae7f685bb11fe40
Austin Miller <Austin@main-10-231-9-165.tamulink.tamu.edu>
2018-10-24 09:10:18 -0500

    Added Level Zone UI

--- VFighter/Assets/Scripts/LevelZoneController.cs
+++ VFighter/Assets/Scripts/LevelZoneController.cs
@@ -9,14 +9,22 @@ public class LevelZoneController : MonoBehaviour {
 
     public string levelName;
     public int playersInside = 0;
+    public Vector3 emptySize;
+    public Vector3 occupiedSize;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>()) ++playersInside;
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>()) --playersInside;
+    }
+
+    private void Update()
+    {
+        transform.localScale = playersInside > 0 ? occupiedSize : emptySize;
     }
 }
