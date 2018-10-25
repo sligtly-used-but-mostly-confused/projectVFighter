4f298d26b51dbc83ab0f3db77e0836e2dba5c631
Ryan Chomistek <ryan.chomistek@tamu.edu>
2018-10-25 17:50:05 -0500

    clean up

--- VFighter/Assets/Scripts/ProjectileControllers/GravityGunProjectileController.cs
+++ VFighter/Assets/Scripts/ProjectileControllers/GravityGunProjectileController.cs
@@ -24,7 +24,6 @@ public class GravityGunProjectileController : NetworkBehaviour {
     
     IEnumerator Onstart () {
         yield return new WaitForSeconds(SecondsUntilDestroy);
-        //NetworkServer.Destroy(gameObject);
         ReturnToPool();
     }
 
