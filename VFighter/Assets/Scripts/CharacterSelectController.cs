bdfef2f3ab9b3d692b8609a9572b833f8e26a166
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-10-26 11:16:42 -0500

    decreased character tip time and improved the gravity objwct shader

--- VFighter/Assets/Scripts/CharacterSelectController.cs
+++ VFighter/Assets/Scripts/CharacterSelectController.cs
@@ -12,7 +12,7 @@ public class CharacterSelectController : NetworkBehaviour {
     public GameObject previewPrefab;
     public GameObject currentIconGameObject;
     public GameObject descriptionPrefab;
-    public int secondForCharacterTip;
+    public float secondForCharacterTip;
 
     private readonly List<PlayerCharacterType> CharacterTypes = Enum.GetValues(typeof(PlayerCharacterType)).Cast<PlayerCharacterType>().ToList();
 
