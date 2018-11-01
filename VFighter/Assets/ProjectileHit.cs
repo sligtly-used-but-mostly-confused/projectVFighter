39bb24fadc4e2ca2b882fc3d45479e0c5a61420f
Juan Carlos Sandoval <sandoval1302@build.tamu.edu>
2018-10-31 21:45:42 -0500

    added collision effect

---
+++ VFighter/Assets/ProjectileHit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileHit : MonoBehaviour {
+    [SerializeField]
+    public BoxCollider2D rd;
+    [SerializeField]
+    public ParticleSystem[] ps;
+   
+    // Use this for initialization
+    void Start () {
+        rd = GetComponent<BoxCollider2D>();
+        ps = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (!ps[1].isPlaying) {
+            ps[1].Stop();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+   
+        var force = collision.relativeVelocity * collision.otherRigidbody.mass;
+       
+            if (force.magnitude > 5.0f)
+            {
+            ps[1].Play();
+              
+            }
+
+    }
+}
