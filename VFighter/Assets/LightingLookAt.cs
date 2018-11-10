ab7cfd3314cfa9fe5e6c8f11be8c265f66e59176
Juan Carlos Sandoval <sandoval1302@build.tamu.edu>
2018-11-09 18:09:05 -0600

    added new line attachment

--- VFighter/Assets/LightingLookAt.cs
+++ VFighter/Assets/LightingLookAt.cs
@@ -3,48 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LightingLookAt : MonoBehaviour {
- 
+
     [SerializeField]
     public float hSliderValue = 0.01f;
     [SerializeField]
-    public GameObject _attachedPlayer;
-    private ParticleSystem ps;
-    
+    public KeyboardPlayerController kp;
+    [SerializeField]
+    protected GameObject prefabLight;
+    [SerializeField]
+    protected ParticleSystem ps;
+    private GameObject LObject;
     // Use this for initialization
+  
+
+
     void Start () {
-        ps = GetComponent<ParticleSystem>();
+        LObject = Instantiate(prefabLight);
+        LObject.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+        DontDestroyOnLoad(LObject);
+        ps = LObject.GetComponent<ParticleSystem>();
+        kp = GetComponent<KeyboardPlayerController>();
     }
 
 
     // Update is called once per frame
     void Update () {
+      
         var main = ps.main;
+        LObject.transform.position = this.transform.position;
         Vector3 positions;
         // Rotate the camera every frame so it keeps looking at the target
-        if (_attachedPlayer)
+        if (kp.AttachedObject)
         {
-            positions = _attachedPlayer.transform.position;
-
+            positions = kp.AttachedObject.transform.position;
+            float dist = Vector3.Distance(LObject.transform.position, positions);
+            main.startLifetime = hSliderValue * dist;
+            // initialize an array the size of our current particle count
+            ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.particleCount];
+            // *pass* this array to GetParticles...
+            int num = ps.GetParticles(particles);
+            for (int i = 0; i < num; i++)
+            {
+                float distParticle = Vector3.Distance(LObject.transform.position, particles[i].position);
+                if (distParticle > dist) // negative x: make it die
+                    particles[i].remainingLifetime = 0;
+            }
+            // re-assign modified array
+            ps.SetParticles(particles, num);
+            LObject.transform.LookAt(kp.AttachedObject.transform.position);
+            ps.Play();
         }
         else
         {
             positions =  Vector3.zero;
+            ps.Stop();
+            print("Distance= " + "NONE");
         }
-        float dist = Vector3.Distance(this.transform.position, positions);
-        main.startLifetime = hSliderValue * dist;
-        // initialize an array the size of our current particle count
-        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.particleCount];
-        // *pass* this array to GetParticles...
-        int num = ps.GetParticles(particles);
-        for (int i = 0; i < num; i++)
-        {
-            float distParticle = Vector3.Distance(this.transform.position, particles[i].position);
-            if (distParticle > dist) // negative x: make it die
-                particles[i].remainingLifetime = 0;
-        }
-        // re-assign modified array
-        ps.SetParticles(particles, num);
-        transform.LookAt(_attachedPlayer.transform.position);
+       
     }
 
 
