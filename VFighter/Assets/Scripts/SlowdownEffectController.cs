5a52c5f002ea43b3efe4829389c3be86b5d3b934
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-11-12 11:21:18 -0600

    added slowdown effect

---
+++ VFighter/Assets/Scripts/SlowdownEffectController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SlowdownEffectController : MonoBehaviour {
+
+    public GravityObjectRigidBody AttachedGORB;
+
+    static bool IsSlowDownCurrentlyRunning = false;
+
+    [SerializeField]
+    private float _slowdownDuration = .5f;
+    [SerializeField]
+    private float _slowdownTimeScale = .1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name + " " + IsSlowDownCurrentlyRunning);
+        if(AttachedGORB.isServer && collision.GetComponent<PlayerController>() && !IsSlowDownCurrentlyRunning)
+        {
+            StartCoroutine(SlowTimeEffect());
+        }
+    }
+
+    public IEnumerator SlowTimeEffect()
+    {
+        IsSlowDownCurrentlyRunning = true;
+        var prevTimeScale = GameManager.Instance.TimeScale;
+        GameManager.Instance.TimeScale = _slowdownTimeScale;
+        yield return new WaitForSeconds(_slowdownDuration);
+        GameManager.Instance.TimeScale = prevTimeScale;
+        IsSlowDownCurrentlyRunning = false;
+    }
+
+    public void OnDestroy()
+    {
+        //make sure the effect doesent get frozen
+        IsSlowDownCurrentlyRunning = false;
+        GameManager.Instance.TimeScale = 1;
+    }
+}
