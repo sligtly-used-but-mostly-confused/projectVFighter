d720b70a7bed1c099ea18d47397859aaf28ae8bb
Evan Bakkal <bakkal27@gmail.com>
2018-11-12 09:11:14 -0600

    Tutorial is correctly connected to main menu and player can select character

---
+++ VFighter/Assets/Scripts/StartTutorialLobby.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class StartTutorialLobby : MonoBehaviour {
+
+    private void Awake()
+    {
+        CustomNetworkManager.Instance.StartHost();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
