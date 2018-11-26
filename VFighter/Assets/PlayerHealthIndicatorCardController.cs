a674624e4ee32fc1e6c49e860fd1cf03ebdf4f9c
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-11-26 11:52:07 -0600

    making timer rotate, also modifactions to the health card

--- VFighter/Assets/PlayerHealthIndicatorCardController.cs
+++ VFighter/Assets/PlayerHealthIndicatorCardController.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using TMPro;
 
 public class PlayerHealthIndicatorCardController : MonoBehaviour {
 
@@ -26,12 +27,15 @@ public class PlayerHealthIndicatorCardController : MonoBehaviour {
 
     public List<CharacterTypeIconPair> IconMappings;
     public Image CharacterIcon;
+    public TextMeshProUGUI PlayerNumberText;
+    public TextMeshProUGUI CharacterTypeText;
 
     private PlayerCharacterType DisplayedCharacterType = PlayerCharacterType.ShotGun;
 
     public void Init(PlayerController player)
     {
         _attachedPlayer = player;
+        PlayerNumberText.text = "P" + _attachedPlayer.PlayerId;
         player.GetComponent<CharacterSelectController>().OnCharacterChanged += OnCharacterTypeChange;
         player.GetComponent<CharacterSelectController>().OnPlayerColorChanged += OnPlayerColorChange;
     }
@@ -81,6 +85,8 @@ public class PlayerHealthIndicatorCardController : MonoBehaviour {
     public void OnPlayerColorChange(Color color)
     {
         CharacterIcon.color = color;
+        CharacterTypeText.color = color;
+        PlayerNumberText.color = color;
     }
 
     public void OnCharacterTypeChange(PlayerCharacterType type)
@@ -88,5 +94,6 @@ public class PlayerHealthIndicatorCardController : MonoBehaviour {
         DisplayedCharacterType = type;
         var pair = IconMappings.Find(x => x.Type == DisplayedCharacterType);
         CharacterIcon.sprite = pair.Icon;
+        CharacterTypeText.text = type.ToString();
     }
 }
--- VFighter/Assets/Scripts/LevelSelectManager.cs
+++ VFighter/Assets/Scripts/LevelSelectManager.cs
@@ -21,6 +21,8 @@ public class LevelSelectManager : MonoBehaviour
     public GameObject levelZone;
     public float areaWidth;
     public TextMeshProUGUI timer;
+    public Image timerClockFace;
+
     public int selectTime;
     public bool IsTimerStarted { get { return _timerCoroutine != null; } }
     public int numLivesPerPlayer;
@@ -36,7 +38,7 @@ public class LevelSelectManager : MonoBehaviour
     [SerializeField]
     private bool _isWaitingForReady = true;
     [SerializeField]
-    private int timeRemaining;
+    private float timeRemaining;
     [SerializeField]
     private int _minPlayers = 2;
     [SerializeField]
@@ -79,9 +81,6 @@ public class LevelSelectManager : MonoBehaviour
 
     private void Update()
     {
-        if(timer != null)
-            timer.text = timeRemaining.ToString();
-
         //if all players are ready start the timer
         if (CheckForAllPlayersReady() && !Instance.IsTimerStarted)
         {
@@ -131,16 +130,29 @@ public class LevelSelectManager : MonoBehaviour
 
     private IEnumerator CountDown()
     {
+        float startingTime = timeRemaining;
+
         if (timer)
-            timer.text = timeRemaining.ToString();
+            timer.text = "" + timeRemaining;
+
+        int valToPass = (int) timeRemaining;
+
         while (timeRemaining > 0){
-            yield return new WaitForSeconds(1);
-            if (timeRemaining > 1)
+            yield return new WaitForEndOfFrame();
+            if (timeRemaining < valToPass)
+            {
                 AudioManager.instance.PlaySingle(countdown);
-            else
-                AudioManager.instance.PlaySingle(countdownFinal);
-            --timeRemaining;
+                valToPass--;
+                timer.text = "" + Mathf.RoundToInt(timeRemaining);
+            }
+
+            timerClockFace.rectTransform.Rotate(new Vector3(0,0, startingTime / timeRemaining), Space.World);
+            
+            timeRemaining -= Time.deltaTime;
         }
+
+        AudioManager.instance.PlaySingle(countdownFinal);
+        timer.text = "0";
 
         yield return new WaitForSeconds(1);
         GameManager.Instance.StartGame(LeadingLevels());
