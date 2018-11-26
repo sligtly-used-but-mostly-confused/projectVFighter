f582cdc665351f0665448ddf6b0d70faf904c578
Ryan Chomistek <ryan.chomistek@tamu.edu>
2018-11-25 20:04:50 -0600

    adding inspector butttons to debug levels without controller

---
+++ VFighter/Assets/Scripts/LevelSelectManagerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelSelectManager))]
+public class LevelSelectManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LevelSelectManager myScript = (LevelSelectManager)target;
+        if (GUILayout.Button("Start Game"))
+        {
+            GameManager.Instance.DebugScene = true;
+            GameManager.Instance.StartGame(myScript.LeadingLevels());
+        }
+    }
+}
