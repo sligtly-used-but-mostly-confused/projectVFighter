8900e067c97f9ab72d59b11a2477aeaef1c007ad
Ryan Chomistek <ryan.chomistek@tamu.edu>
2018-11-26 19:28:56 -0600

    changed transition material

---
+++ VFighter/Assets/TransitionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TransitionController : MonoBehaviour {
+
+	public void StartTransition(Action OnFinish)
+    {
+        StartCoroutine(PlayAndWaitForVideoToEnd(OnFinish));
+    }
+
+    IEnumerator PlayAndWaitForVideoToEnd(Action OnFinish)
+    {
+        GetComponent<Renderer>().enabled = true;
+        var player = GetComponent<VideoPlayer>();
+        //player.clip.length
+        player.Play();
+        while (player.isPlaying)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        GetComponent<Renderer>().enabled = false;
+        OnFinish();
+    }
+}
