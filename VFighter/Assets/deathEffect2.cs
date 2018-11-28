df35271f5a9581a09722a46b112a392f39c852ec
Juan Carlos <sandoval1302@tamu.edu>
2018-11-28 10:40:56 -0600

    death effect almsot done

---
+++ VFighter/Assets/deathEffect2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathEffect2 : MonoBehaviour {
+    [SerializeField]
+    public BoxCollider2D bc;
+    [SerializeField]
+    public ParticleSystem ps;
+
+    // Use this for initialization
+    void Start () {
+        bc = GetComponentInParent<BoxCollider2D>();
+        ps = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ps.Play();
+    }
+}
