cec50fb3c73364e782fa67a1e213fd5a3b2baf13
Juan Carlos Sandoval <sandoval1302@build.tamu.edu>
2018-11-28 18:41:11 -0600

    added stuff

---
+++ VFighter/Assets/pHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pHit : MonoBehaviour {
+    [SerializeField]
+    public BoxCollider2D bc;
+    [SerializeField]
+    public ParticleSystem[] ps;
+    ParticleSystem.MainModule main;
+    // Use this for initialization
+    void Start () {
+        bc = GetComponent<BoxCollider2D>();
+        ps = GetComponentsInChildren<ParticleSystem>();
+        main = ps[3].main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hit");
+    }
+
+}
