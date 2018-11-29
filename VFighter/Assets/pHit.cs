0dcd7995a56b6cd73292784bdbc0c3390a5f9fa5
Juan Carlos Sandoval <sandoval1302@build.tamu.edu>
2018-11-28 22:33:29 -0600

    changed death effect and projecttiel

--- VFighter/Assets/pHit.cs
+++ VFighter/Assets/pHit.cs
@@ -8,21 +8,35 @@ public class pHit : MonoBehaviour {
     [SerializeField]
     public ParticleSystem[] ps;
     ParticleSystem.MainModule main;
+    ParticleSystem.EmissionModule em;
+  
     // Use this for initialization
     void Start () {
         bc = GetComponent<BoxCollider2D>();
         ps = GetComponentsInChildren<ParticleSystem>();
-        main = ps[3].main;
-	}
+        main = ps[2].main;
+        em = ps[2].emission;
+        em.enabled = true;
+        main.startSize = 0.0f;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        
-	}
+	
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("hit");
+        var force = collision.relativeVelocity * collision.otherRigidbody.mass;
+
+        if (force.magnitude > 1.0f)
+        {
+            // em.enabled = true;
+            main.startSize = 0.4f;
+            main.simulationSpeed = 1f;
+            ps[2].Emit(50);
+     
+        }
     }
+
+ 
 
 }
