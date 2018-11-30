3b99418f4bd91e095bbbe3dfd0229f438e4b8af3
Juan Carlos Sandoval <sandoval1302@build.tamu.edu>
2018-11-30 15:09:17 -0600

    astroids to backgrpound erasy level

--- VFighter/Assets/MetShower.cs
+++ VFighter/Assets/MetShower.cs
@@ -20,15 +20,17 @@ public class MetShower : MonoBehaviour
             Rigidbody2D rd;
             GameObject tempObject;
             MeshRenderer mr;
-            int height = Random.Range(120, 90);
-            int width = Random.Range(120, 80);
+            int height = Random.Range(110, 90);
+            int width = Random.Range(120, 85);
             tempObject = Instantiate(gb);
             tempObject.transform.localPosition = new Vector3(width, height, 0);
             astroArray.Add(tempObject);
+            float randScale = Random.Range(0.1f, 0.01f);
+            astroArray[i].transform.localScale = new Vector3(randScale, randScale, randScale);
             rd = astroArray[i].GetComponent<Rigidbody2D>();
             mr = astroArray[i].GetComponent<MeshRenderer>();
             mr.enabled = false;
-            rd.AddForce(new Vector2(-0.2f, 0.5f), ForceMode2D.Impulse);
+            rd.AddForce(new Vector2(Random.Range(-0.1f, -1f), Random.Range(0.1f, 0.3f)), ForceMode2D.Impulse);
         }
     }
 
@@ -39,7 +41,7 @@ public class MetShower : MonoBehaviour
       
         for (int i = 0; i < numAstro; ++i)
         {
-            if (astroArray[i].transform.position.x <= 80f || astroArray[i].transform.position.y >= 120f)
+            if (astroArray[i].transform.position.x <= 85f || astroArray[i].transform.position.y > 110f || astroArray[i].transform.position.y <= 85f)
             {
                 Rigidbody2D rd;
                 MeshRenderer mr;
@@ -48,9 +50,11 @@ public class MetShower : MonoBehaviour
                 int height = Random.Range(120, 90);
                 float forceVertical = Random.Range(-1, -2);
                 astroArray[i].transform.position = new Vector3(120f, height, 0f);
+                float randScale = Random.Range(0.1f, 0.01f);
+                astroArray[i].transform.localScale = new Vector3(randScale, randScale, randScale);
                 rd = astroArray[i].GetComponent<Rigidbody2D>();
                 rd.velocity = Vector3.zero;
-                rd.AddForce(new Vector2(-0.2f, 0.5f), ForceMode2D.Impulse);
+                rd.AddForce(new Vector2(Random.Range(-0.1f,-1f), Random.Range(0.1f,0.3f)), ForceMode2D.Impulse);
             }
         }
     }
