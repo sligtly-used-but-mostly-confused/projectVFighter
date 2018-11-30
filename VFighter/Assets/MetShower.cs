a2e19f689ad55c830d8e6415ccc5a931e5c1e72c
Juan Carlos <sandoval1302@tamu.edu>
2018-11-30 12:45:58 -0600

    added stuff

--- VFighter/Assets/MetShower.cs
+++ VFighter/Assets/MetShower.cs
@@ -2,39 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MetShower : MonoBehaviour {
+public class MetShower : MonoBehaviour
+{
     [SerializeField]
     public GameObject gb;
     [SerializeField]
     public int numAstro = 0;
     [SerializeField]
-    private List<GameObject> temp;
-   
+    private List<GameObject> astroArray;
+
     // Use this for initialization
-    void Start () {
-       
-        for(int i = 0; i < numAstro; ++i)
+    void Start()
+    {
+
+        for (int i = 0; i < numAstro; ++i)
         {
             Rigidbody2D rd;
             GameObject tempObject;
-            int height = Random.Range(0, 5);
-            int width = Random.Range(0, 5);
+            MeshRenderer mr;
+            int height = Random.Range(120, 90);
+            int width = Random.Range(120, 80);
             tempObject = Instantiate(gb);
             tempObject.transform.localPosition = new Vector3(width, height, 0);
-            temp.Add(tempObject);
-            rd = temp[i].GetComponent<Rigidbody2D>();
-            rd.AddForce(new Vector2(-15f,2f),ForceMode2D.Impulse);
-
+            astroArray.Add(tempObject);
+            rd = astroArray[i].GetComponent<Rigidbody2D>();
+            mr = astroArray[i].GetComponent<MeshRenderer>();
+            mr.enabled = false;
+            rd.AddForce(new Vector2(-0.2f, 0.5f), ForceMode2D.Impulse);
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-          for(int i = 0; i < 10; ++i)
-        {
-            if (temp[i])
-            {
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+
+      
+        for (int i = 0; i < numAstro; ++i)
+        {
+            if (astroArray[i].transform.position.x <= 80f || astroArray[i].transform.position.y >= 120f)
+            {
+                Rigidbody2D rd;
+                MeshRenderer mr;
+                mr = astroArray[i].GetComponent<MeshRenderer>();
+                mr.enabled = true;
+                int height = Random.Range(120, 90);
+                float forceVertical = Random.Range(-1, -2);
+                astroArray[i].transform.position = new Vector3(120f, height, 0f);
+                rd = astroArray[i].GetComponent<Rigidbody2D>();
+                rd.velocity = Vector3.zero;
+                rd.AddForce(new Vector2(-0.2f, 0.5f), ForceMode2D.Impulse);
             }
         }
     }
